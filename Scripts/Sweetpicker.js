48ca540c1b44a3a2b47994fb5ea49a701aafe1a1
ernest asokwonye <mistyravenn@gmail.com>
2023-09-14 18:25:59 +0100

    updated code

--- Scripts/Sweetpicker.js
+++ Scripts/Sweetpicker.js
@@ -18,8 +18,79 @@ const sweetPicker = () => {
       invalidChoice();
       continue;
     }
-  }
-  if (!customerChoice) {
-    noChocolate();
+    if (!customerChoice) {
+      noChocolate();
+      break;
+    }
+    customerChoice = checkCustomerChoice(customerChoice);
+    if (!customerChoice) {
+      invalidChoice();
+      continue;
+    }
+    const chocolateStock = getChocolateStock();
+    const output = informCustomer(customerChoice, chocolateStock);
+    showOutput(output);
+    if (selectAgain(false)) {
+      continue;
+    } else {
+      thanksForShopping();
+      break;
+    }
   }
 };
+
+const getCustomerInput = () => {
+  return prompt("Please select from our chocolate samples A, B , C and D");
+};
+
+const formatCustomerInput = (customerChoice) => {
+  if (customerChoice || customerChoice === "") {
+    return customerChoice.trim().toUpperCase();
+  } else {
+    return false;
+  }
+};
+
+const invalidChoice = () => {
+  alert("You didn't select from our list.");
+  return selectAgain();
+};
+
+const noChocolate = () => {
+  alert("You'll get our plain chocolate");
+};
+
+const checkCustomerChoice = (customerChoice) => {
+  if (
+    customerChoice === "A" ||
+    customerChoice === "B" ||
+    customerChoice === "C" ||
+    customerChoice === "D"
+  ) {
+    return customerChoice;
+  } else {
+    return false;
+  }
+};
+
+const thanksForShopping = () => {
+  alert("Thank you for shopping with Us");
+};
+
+const selectAgain = () => {
+  confirm("Enter a valid chocolate sample")
+}
+
+const getChocolateStock = () => {
+  const randomizedsweet = Math.floor(Math.random() * 4);
+  const Sweetgroup = [
+    "Stewberry Sweet",
+    "Vanilla Sweet",
+    "Apple & Milk Sweet",
+    "Banana Sweet",
+  ];
+  return Sweetgroup[randomizedsweet];
+};
+
+
+
